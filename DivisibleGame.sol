fac175d5af8fd5c662b072ade96f0c759eb3e895
xMrhyd3 <cmccoyrei@gmail.com>
2016-10-07 12:54:29 -0400

    Working
    Working on draft.

--- DivisibleGame.sol
+++ DivisibleGame.sol
@@ -8,22 +8,23 @@ struct Game {
   uint256 public numberChosen1;
   uint256 public amountWagered;
   bool public isDivisible;
+  address public player2;
+  uint256 numberChosen2;
 }
 
-uint numGames;
+uint256 numGames;
 
 mapping (uint => Game) Games;
-
 
 
   function newGame(address player1, uint amountBet, uint numberChosen, bool isDivisible) returns (uint gameID) 
   {
       if (msg.value >= 100 finney && numberChosen > 0 && numberChosen < 10) {
       
-      uint amountbet = msg.value;
+      uint amount = msg.value;
       
       gameID = numGames++;
-      games[gameID] = Game(player1, numberChosen, amountBet, isDivisible);
+      games[gameID] = Game(player1, numberChosen, amount, isDivisible,0,0);
   }
      
      
