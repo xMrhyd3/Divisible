be01e916fcc1d01edf2dcd38af73581aff329400
xMrhyd3 <cmccoyrei@gmail.com>
2016-10-06 22:01:38 -0400

    Just Getting Started
    Very early stages but overall this should be a fairly simple and straight forward smart contract.  More updates coming this weekend!

--- DivisibleGame.sol
+++ DivisibleGame.sol
@@ -1,28 +1,49 @@
 contract DivisibleGame {
-  address public playerA;
-  address public playerB;
-  uint256 public numberChosenA;
-  uint256 public numberChosenB;
-  uint256 public amountWagered
-  
-  function createGame(
-    uint256 numberPickedA,
-    uint256 amountBet,
-    address addressPlayerA
-  ) {
-      playerA = addressPlayerA;
-      numberPickedA = numberChosenA;
-      amountWagered = amountBet;
-     }
+    address public owner;
+}
+
+
+struct Game {
+  address public player1;
+  uint256 public numberChosen1;
+  uint256 public amountWagered;
+  bool public isDivisible;
+}
+
+uint numGames;
+
+mapping (uint => Game) Games;
+
+
+
+  function newGame(address player1, uint amountBet, uint numberChosen, bool isDivisible) returns (uint gameID) 
+  {
+      if (msg.value >= 100 finney && numberChosen > 0 && numberChosen < 10) {
+      
+      uint amountbet = msg.value;
+      
+      gameID = numGames++;
+      games[gameID] = Game(player1, numberChosen, amountBet, isDivisible);
+  }
      
-   function playGame(
-     uint256 numberPickedB,
-     uint256 amountBet,
-     address addressPlayerB,
-   ) {
-        playerB = addressPlayerB;
-        numberChosenB = numberPickedB;
-        
+     
+  function playGame(address player2, uint amountBet, uint numberChosen2, uint gameID)
+  {
+      Game = games[gameID];
+      
+      if (msg.value == Game.amountBet && numberChosen > 0 && numberChosen < 10) 
+      
+        if ( Game.isDivisible == True && (numberChosen1 + numberChosen2) % 2 == 0)
+              msg.sender.send(amountBet)
+  }   
+      
+      
+  
+  
+  
+modifier onlyOwner { if(msg.sender == contractOwner) _; }
+
+event GameEnded (address Winner, uint256 TotalBet)
         
      
      
