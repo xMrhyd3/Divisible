7b49c68b52c0b224610c10a94ece18bd1a344dd0
xMrhyd3 <cmccoyrei@gmail.com>
2016-10-13 13:12:01 -0400

    Added comments and tip variable
    Added the following:
    - Comments to help give more detail around variables and functions.
    - amountTip to the Game struct.  This tip amount will be paid to the developers and amount is completely up to the user. 
    - Second possible outcome for player 1 to win,  they bet false and the outcome is false.  Prior to this only win and win was included.

--- DivisibleGame.sol
+++ DivisibleGame.sol
@@ -1,30 +1,31 @@
 contract DivisibleGame {
     address owner;
-
+    
+//This is where the game details will be stored.  
 struct Game {
-  address player1;
-  uint numberChosen1;
-  uint amountWagered;
-  bool isDivisible;
-  address player2;
-  uint numberChosen2;
-  bool gameOver;
+  address player1;      //Person who creates the game
+  uint numberChosen1;   //Number between 1-10 that the game creator (Player 1) chooses
+  uint amountWagered;   //Amount the game creator chooses to bet, when a player joins the game they will send in the same amount 
+  bool isDivisible;     //Player 1 chooses whether the outcome will be divisible by 2 (True) or not divisible by 2 (False)
+  address player2;      //Person joining a game
+  uint numberChosen2;   //Number between 1-10 that the person joining a game (Player 2) chooses
+  bool gameOver;        //Flag to indicate the game is over.  This prevents the Game Details function being used prior to a game finishing.    
+  uint amountTip;      //Game creator can choose if tip will be paid to developers.  Tip is a percentage of winning amount.
 }
 
 
-uint numGames = 0;
+uint numGames = 0;      //Keeps track of the number of games and is used to provide the index number to the mapping
 
-mapping (uint => Game) Games;
+mapping (uint => Game) Games; //Essentially assigns the Game to a Game ID
 
-modifier gameOver(uint gameID)
+modifier gameOver(uint gameID) //Used to prevent certain functions from being run prior to a game finishing
 {
     if (Games[gameID].gameOver == false) 
-    _ 
-    
+    _    
 }
 
-
-  function newGame( uint numberChosen, bool _isDivisible) returns (uint gameID) 
+//Creates a new game
+  function newGame( uint numberChosen, bool _isDivisible, uint _amountTip) returns (uint gameID) 
   {
       if (numberChosen > 0 && numberChosen < 10) 
       {
@@ -32,12 +33,12 @@ modifier gameOver(uint gameID)
         uint _amountBet = msg.value;
       
         gameID = numGames++;
-        Games[gameID] = Game( _player1, numberChosen, _amountBet, _isDivisible,0,0,false);
+        Games[gameID] = Game( _player1, numberChosen, _amountBet, _isDivisible,0,0,false, _amountTip);
         return gameID;
       }
   }
 
-
+//Allows player to 'join' a game
   function playGame(uint numberChosen, uint gameID) gameOver(gameID)
   {
      if (msg.value == Games[gameID].amountWagered && numberChosen > 0 && numberChosen < 10)
@@ -62,24 +63,32 @@ modifier gameOver(uint gameID)
      
      endGame(isDivisibleResult, gameID);
      }    
-     
+      
         
   } 
-     
+
+//Private function the contract will call when it gets to the end of the playGame function.  Pays out winner.
   function endGame(bool isDivisibleResult, uint gameID) private
   {
-    address player1 = Games[gameID].player1;
-    address player2 = Games[gameID].player2;
-    uint amountWon = Games[gameID].amountWagered;
+    address _player1 = Games[gameID].player1;
+    address _player2 = Games[gameID].player2;
+    uint amountWon = Games[gameID].amountWagered - (Games[gameID].amountWagered * (Games[gameID].amountTip/100))
+    uint amountTipped = Games[gameID].amountWagered * (Games[gameID].amountTip/100);
     bool betDivisible = Games[gameID].isDivisible;
     
       if (isDivisibleResult == true && betDivisible == true)
       {
-          if (player1.send(amountWon)==false) throw;
-      } else if (player2.send(amountWon)==false)throw;
+          if (_player1.send(amountWon)==false) throw;
+      } else if (_player2.send(amountWon)==false)throw;
+      
+      if (isDivisibleResult == false && betDivisible == false)
+      {
+         if (_player1.send(amountWon)==false) throw;
+      } else if (_player2.send(amountWon)==false)throw;
       
   }
   
+//Returns details of a specific game based on the Game ID given to the function.  Mainly for testing purposes right now.  
   function getGameDetails(uint gameID) gameOver(gameID) constant returns(address player1, uint numberChosen1, uint amountWagered, address player2, uint numberChosen2)
   {
      player1 = Games[gameID].player1;
@@ -92,12 +101,14 @@ modifier gameOver(uint gameID)
      return (player1, numberChosen1, amountWagered, player2, numberChosen2);
   }
   
+//Returns the number of games that have been created so far.  Mainly for testing purposes right now.  
   function getNumberOfGames() constant returns (uint _numGames)
   {
       _numGames = numGames;
       return;
   }
   
+//Destroys contract.
   function kill() {if (msg.sender == owner) selfdestruct(owner);}
 
   }
